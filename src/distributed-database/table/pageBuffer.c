5643496afd05ca30f26b892e0f3213c875a0a378
dfilip <cf924@ic.ac.uk>
2025-09-17 09:18:44 +0100

    Implement main buffer functionality

--- src/distributed-database/table/pageBuffer.c
+++ src/distributed-database/table/pageBuffer.c
@@ -1,16 +1,18 @@
 #include "pageBuffer.h"
-
+#include <assert.h>
 #include <stdbool.h>
 #include "int-hashmap.h"
 
 #define BUFFER_POOL_SIZE 64
 #define MAX_USAGE 5
+#define EVICT_USAGE 0
+#define INITIAL_CLOCK 0
+#define INITIAL_USAGE 1
 
 struct Frame {
-    size_t pageId;
     size_t usageIdx;
     bool dirty;
-    struct Page page;
+    Page page;
 };
 typedef struct Frame Frame;
 
@@ -20,3 +22,58 @@ struct Buffer {
     IntHashmap frameMap;
 };
 
+Frame *getFrame(Buffer buffer);
+void loadPageToFrame(Page page, Frame *frame);
+
+Buffer initialiseBuffer() {
+    Buffer buffer = malloc(sizeof(struct Buffer));
+    assert(buffer != NULL);
+
+    buffer->clock = INITIAL_CLOCK;
+    buffer->frameMap = createIntHashmap(NULL);
+    assert(buffer->frameMap != NULL);
+
+    return buffer;
+}
+
+void freeBuffer(Buffer buffer) {
+    freeIntHashmap(buffer->frameMap);
+    free(buffer);
+}
+
+Page loadPage(TableInfo tableInfo, Buffer buffer, size_t pageId) {
+    Frame *frame = getIntHashmap(buffer->frameMap, pageId);
+
+    if (frame == NULL) {
+        // Cache miss
+        Page page = getPage(tableInfo, pageId);
+        Frame *newFrame = getFrame(buffer);
+        loadPageToFrame(page, newFrame);
+        addIntHashmap(buffer->frameMap, pageId, newFrame);
+        return page;
+    }
+
+    if (frame->usageIdx < MAX_USAGE) frame->usageIdx++;
+
+    return frame->page;
+}
+
+Frame *getFrame(Buffer buffer) {
+    size_t *clock = &buffer->clock;
+
+    while (buffer->frames[*clock].usageIdx != EVICT_USAGE) {
+        buffer->frames[*clock].usageIdx--;
+        *clock++;
+        if (*clock == BUFFER_POOL_SIZE) {
+            *clock = INITIAL_CLOCK;
+        }
+    }
+
+    return &buffer->frames[*clock];
+}
+
+void loadPageToFrame(Page page, Frame *frame) {
+    frame->usageIdx = INITIAL_USAGE;
+    frame->page = page;
+    frame->dirty = false;
+}
--- src/distributed-database/table/pageBuffer.h
+++ src/distributed-database/table/pageBuffer.h
@@ -1,13 +1,30 @@
 #ifndef PAGEBUFFER_H
 #define PAGEBUFFER_H
-#include <stddef.h>
 
+#include <stddef.h>
 #include "pages.h"
 
 typedef struct Buffer *Buffer;
 
+/**
+ *
+ * @return Buffer with clock set to 0 and empty frame pool initialised
+ */
 extern Buffer initialiseBuffer();
+
+/**
+ *
+ * @param buffer Buffer to be freed
+ */
 extern void freeBuffer(Buffer buffer);
-extern struct Page loadPage(size_t loadPage);
+
+/**
+ *
+ * @param tableInfo Contains database file to load pages from
+ * @param buffer Buffer pool of pages in use
+ * @param pageId ID of page to be loaded
+ * @return Page returned for use
+ */
+extern Page loadPage(TableInfo tableInfo, Buffer buffer, size_t pageId);
 
 #endif
