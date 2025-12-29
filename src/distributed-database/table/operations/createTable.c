6fbafd220435d1408fd14c0a5f21fbd3174dcf83
dfilip <cf924@ic.ac.uk>
2025-12-29 10:13:07 +0000

    [cf924] refactor(createTable): Use sql commands for insertion into schema table

--- src/distributed-database/table/operations/createTable.c
+++ src/distributed-database/table/operations/createTable.c
@@ -10,122 +10,75 @@
 #include "../schema.h"
 #include "insert.h"
 #include "log.h"
+#include "sqlToOperation.h"
 #include "table/core/table.h"
 
-static void setSchema(char *schema, char *tableName, QueryAttributes attributes,
-                      const unsigned int *sizes, QueryTypes types) {
-    LOG("SET SCHEMA\n");
-    TableInfo schemaInfo = openTable(schema);
+static void setSchema(char *tableName, QueryTypes descriptors) {
+    // Creates schema table
+    char schemaName[MAX_FILE_NAME_LEN];
+    snprintf(schemaName, MAX_FILE_NAME_LEN, "%s-schema", tableName);
+    initialiseTable(schemaName);
+
+    TableInfo schemaInfo = openTable(schemaName);
 
     // Gets data dictionary schema
     Schema *dictSchema = initDictSchema();
+
     // Set length of relation name to length of table name
     dictSchema->attributeSizes[RELATION_NAME_IDX] = strlen(tableName);
 
-    // Initialises parameters for insertion into schema
-    int initialIdx = 0;
-    Operand nameOp = createOperand(STR, tableName);
-    Operand typeOp = createOperand(INT, &initialIdx);
-    Operand idxOp = createOperand(INT, &initialIdx);
-    Operand sizeOp = createOperand(INT, &initialIdx);
-    Operand attNameOp = createOperand(VARSTR, "");
+    // Insertion command template
+    char template[] = "insert into %s values (%s, %d, %d, %d, %s);";
+    char sql[500];
 
-    QueryValues values =
-        createQueryValues(5, nameOp, typeOp, idxOp, sizeOp, attNameOp);
-
-    QueryAttributes dictAttributes = createQueryAttributes(
-        5, SCHEMA_RELATION_NAME, SCHEMA_ATTRIBUTE_TYPE, SCHEMA_IDX,
-        SCHEMA_ATTRIBUTE_SIZE, SCHEMA_ATTRIBUTE_NAME);
-
-    // Inserts hidden global index as first attribute
+    // Index of attribute in schema
     int idx = 0;
 
-    // Inserts non-variable length fields with index
-    for (int i = 0; i < attributes->numAttributes; i++) {
-        AttributeName attribute = attributes->attributes[i];
-        if (types->types[i] != VARSTR) {
-            idxOp->value.intOp = idx;
-            typeOp->value.intOp = types->types[i];
+    unsigned numTypes = descriptors->numTypes;
 
-            assert(sizes[i] <= INT_MAX);
-            sizeOp->value.intOp = sizes[i];
-
-            attNameOp->value.strOp = attribute;
-
-            // Inserts record for attribute info into schema
-            LOG("INSERTING ATTRIBUTE %s WITH TYPE %d\n", attribute,
-                types->types[i]);
-            insertInto(schemaInfo, NULL, dictSchema, dictAttributes, values,
-                       SCHEMA);
+    // Inserts fixed-length fields with index
+    for (int i = 0; i < numTypes; i++) {
+        QueryTypeDescriptor type = descriptors->types[i];
+        if (type->type != VARSTR) {
+            snprintf(sql, sizeof(sql), template, schemaName, type->type, idx, type->size, type->name);
+            insertOperation(schemaInfo, NULL, dictSchema, sqlToOperation(sql));
             idx++;
         }
     }
 
-    // Inserts variable length fields
-    for (int i = 0; i < attributes->numAttributes; i++) {
-        AttributeName attribute = attributes->attributes[i];
-        if (types->types[i] == VARSTR) {
-            typeOp->value.intOp = types->types[i];
-            idxOp->value.intOp = idx;
-            attNameOp->value.strOp = attribute;
-
-            assert(sizes[i] <= INT_MAX);
-            sizeOp->value.intOp = sizes[i];
-
-            LOG("INSERTING ATTRIBUTE %s WITH TYPE %d\n", attribute,
-                types->types[i]);
-            insertInto(schemaInfo, NULL, dictSchema, dictAttributes, values,
-                       SCHEMA);
+    // Inserts variable-length fields with index
+    for (int i = 0; i < numTypes; i++) {
+        QueryTypeDescriptor type = descriptors->types[i];
+        if (type->type == VARSTR) {
+            snprintf(sql, sizeof(sql), template, schemaName, type->type, idx, type->size, type->name);
+            insertOperation(schemaInfo, NULL, dictSchema, sqlToOperation(sql));
             idx++;
         }
     }
 
-    for (int i = 0; i < values->numValues; i++) {
-        free(values->values[i]);
-    }
-    free(values->values);
-    free(values);
-    freeQueryAttributes(dictAttributes);
     free(dictSchema);
     closeTable(schemaInfo);
 }
 
 void createTable(Operation operation) {
-    LOG("Execute create table\n");
     initialiseTable(operation->tableName);
 
-    // Creates schema table
-    char buffer[MAX_FILE_NAME_LEN];
-    snprintf(buffer, MAX_FILE_NAME_LEN, "%s-schema", operation->tableName);
-    initialiseTable(buffer);
+    unsigned numTypes = operation->query.createTable.types->numTypes;
 
-    // Type for each attribute
-    // unsigned int
-    // typeSizes[operation->query.createTable.attributes->numAttributes]; Index
-    // of variable length field for sizes array
-    int varFieldIdx = 0;
-
-    // for (int i = 0; i < operation->query.createTable.types->numTypes;
-    //    i++) {
-    //   const AttributeType type =
-    //   operation->query.createTable.types->types[i];
-    // if (type == VARSTR || type == STR) {
-    //        typeSizes[i] =
-    //            operation->query.createTable.types->sizes[varFieldIdx];
-    //    varFieldIdx++;
-    //    } else {
-    //      typeSizes[i] = getStaticTypeWidth(type);
-    //    }
-    // LOG("Type size %d: %d", i, typeSizes[i]);
-    // }
+    // Sets size of static non-string fields
+    for (int i = 0; i < numTypes; i++) {
+        QueryTypeDescriptor type = operation->query.createTable.types->types[i];
+        if (type->type != VARSTR && type->type != STR) {
+            type->size = getStaticTypeWidth(type->type);
+        }
+    }
 
     // Writes the schema of the table
-    // setSchema(buffer, operation->tableName,
-    //          operation->query.createTable.attributes, typeSizes,
-    //          operation->query.createTable.types);
+    setSchema(operation->tableName, operation->query.createTable.types);
 
-    snprintf(buffer, MAX_FILE_NAME_LEN, "%s-space-inventory",
+    char *spaceTableName[MAX_FILE_NAME_LEN];
+    snprintf(spaceTableName, MAX_FILE_NAME_LEN, "%s-space-inventory",
              operation->tableName);
 
-    initialiseTable(buffer);
+    initialiseTable(spaceTableName);
 }
